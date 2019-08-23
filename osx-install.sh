0f742f69bb6136c7048c9f3a1884d70117d06c20
Michael Bianchi <mbianchi@opploans.com>
2019-08-23 11:23:14 -0500

    Install script

---
+++ osx-install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+brew install python
+pip3 install awscli --upgrade --user
+
+
+# AWS Session Manager Plugin
+curl "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/mac/sessionmanager-bundle.zip" -o "sessionmanager-bundle.zip"
+unzip sessionmanager-bundle.zip
+sudo ./sessionmanager-bundle/install -i /usr/local/sessionmanagerplugin -b /usr/local/bin/session-manager-plugin
+
+# Install into PATH (/usr/local/bin optional, any location in your $PATH will work)
+cp ./olysseum /usr/local/bin/olysseum
+
